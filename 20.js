9124694f9042e3550d1ee77dcef8531d01de312a
Sylvain Sénéchal <senechalsylvain@hotmail.com>
2019-03-10 05:33:22 +0100

    Create 20.js

---
+++ 20.js
@@ -0,0 +1,7 @@
+// const factorial = n => n === 1 ? 1 : n*factorial(n-BigInt(1))
+
+const tailFact = (n, acc = BigInt(1)) => n == 1 ? acc : tailFact(n - BigInt(1), acc*n)
+
+let results = tailFact(BigInt(100)).toString().split('').map( digit => parseInt(digit)).reduce( (acc, value) => acc + value )
+
+console.log(results)
